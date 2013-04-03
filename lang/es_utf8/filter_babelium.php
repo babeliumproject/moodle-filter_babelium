da544be689d9e3c6161fae0b0905782f1f0e70fa
Inko Perurena <inkoperurena@gmail.com>
2013-04-03 13:46:20 +0200

    Remove metadata, add filter plugin for moodle 1.9.x and moodle 2.0.x-2.2.x versions. Added compatibility for filter/non-filter modes in 1.9 plugin version.

---
+++ lang/es_utf8/filter_babelium.php
@@ -0,0 +1,39 @@
+<?php 
+
+// This file is part of Babelium - http://babeliumproject.com/
+//
+// Babelium is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Babelium is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Babelium.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'filter_babelium', language 'es'
+ *
+ * @package   filter_babelium
+ * @copyright 2012 Babelium Project {@link http://babeliumproject.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['filtername'] = 'Babelium';
+
+$string['serverdomain'] = 'Dominio del servidor';
+$string['serverdomain_help'] = 'El dominio del servidor Babelium en el que se guardaran tanto los ejercicios como las grabaciones';
+$string['serverport'] = 'Puerto del servidor';
+$string['serverport_help'] = 'El número de puerto por el que se accede al servidor';
+$string['apidomain'] = 'Dominio del API';
+$string['apidomain_help'] = 'El dominio del API-RPC que permite al usuario acceder a los datos almacenados en el servidor Babelium';
+$string['apiendpoint'] = 'Endpoint del API';
+$string['apiendpoint_help'] = 'Una ruta que pertenece al dominio del API y que se usa para gestionar todas las peticiones';
+$string['accesskey'] = 'Clave de acceso';
+$string['accesskey_help'] = 'Una clave única que identifica todas las peticiones que el usuario realiza al API-RPC del servidor Babelium';
+$string['secretaccesskey'] = 'Clave de acceso secreta';
+$string['secretaccesskey_help'] = 'La clave secreta se utiliza para validar la identidad del usuario en las peticiones. Nunca debe usarse sin algún mecanismo encriptación en scripts locales';
---
+++ lang/eu_utf8/filter_babelium.php
@@ -0,0 +1,39 @@
+<?php 
+
+// This file is part of Babelium - http://babeliumproject.com/
+//
+// Babelium is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Babelium is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Babelium.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'filter_babelium', language 'eu'
+ *
+ * @package   filter_babelium
+ * @copyright 2012 Babelium Project {@link http://babeliumproject.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['filtername'] = 'Babelium';
+
+$string['serverdomain'] = 'Zerbitzari domeinua';
+$string['serverdomain_help'] = 'Ariketak zein grabazioak gordetzeko erabiliko den Babelium zerbitzariaren domeinua';
+$string['serverport'] = 'Zerbitzari portua';
+$string['serverport_help'] = 'Zerbitzarira atzitzeko erabiltzen den portu zenbakia';
+$string['apidomain'] = 'API domeinua';
+$string['apidomain_help'] = 'Erabiltzaileari Babelium zerbitzarian dauden datuak atzitzea ahalbidetzen duen RPC-APIaren domeinua';
+$string['apiendpoint'] = 'APIaren endpointa';
+$string['apiendpoint_help'] = 'Eskaera guztiak kudeatzeko erabiltzen den APIaren domeinuaren barneko helbidea';
+$string['accesskey'] = 'Atzipen gakoa';
+$string['accesskey_help'] = 'Erabiltzaileak Babelium zerbitzariaren aurka RPC-APIaren bitartez egiten dituen eskaera guztiak identifikatzen dituen gako unibokoa';
+$string['secretaccesskey'] = 'Atzipen gako sekretua';
+$string['secretaccesskey_help'] = 'Eskaeretan erabiltzailearen identitatea balioztatzeko erabiltzen da gako sekretua. Ez da inoiz skript lokaletan enkriptatu gabe erabili behar';
