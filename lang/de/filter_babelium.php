e07e39eceabfe671def64359b90e8dd179c272b7
Inko Perurena <inkoperurena@gmail.com>
2013-06-27 13:28:29 +0200

    Added contributed German locale to the different plugin versions.

---
+++ lang/de/filter_babelium.php
@@ -0,0 +1,40 @@
+<?php 
+
+// This file is part of Babelium - http://babeliumproject.com/
+//
+// Babelium is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Babelium is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Babelium.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'filter_babelium', language 'de'
+ *
+ * @package   filter_babelium
+ * @copyright 2012 Babelium Project {@link http://babeliumproject.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['filtername'] = 'Babelium';
+
+$string['serverdomain'] = "Server Domain";
+$string['serverdomain_help'] = "Die Babelium Server Domain, in der beide Übungen wie Aufnahmen gespeichert werden";
+$string['serverport'] = "Server-Port";
+$string['serverport_help'] = "Die Port-Nummer, mit der Sie auf den Server zugreifen";
+$string['apidomain'] = "Domain API";
+$string['apidomain_help'] = "Die Domain-RPC API, mit dem Benutzer auf die  auf dem Server gespeicherten Daten von Babelium zugreifen können";
+$string['apiendpoint'] = "API Endpunkt";
+$string['apiendpoint_help'] = "Eine Route, die zu der Domain des API gehört, und verwendet wird, um alle Anfragen zu verwalten";
+$string['accesskey'] = "Access Key";
+$string['accesskey_help'] = "Ein eindeutiger Schlüssel, der alle Anfragen, die der Benutzer über den Server-RPC API Babelium durchführt identifiziert";
+$string['secretaccesskey'] = "Geheimer Access Key";
+$string['secretaccesskey_help'] = "Der geheime Schlüssel wird verwendet, um die Identität des Benutzers bei den Anfragen zu verifizieren. Niemals ohne Verschlüsselungsmechanismus in lokalen Skripten 'zu verwenden";
+
